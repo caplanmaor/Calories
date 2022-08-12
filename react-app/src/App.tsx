699b267877fb4303bf387a4f5f888d8581dd54e4
caplanmaor <caplanmaor@gmail.com>
2022-08-12 15:35:38 +0300

    auto-complete with white bg and add button

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import './App.css';
+import Button from '@mui/material/Button';
+import Autocomplete from '@mui/material/Autocomplete';
+import TextField from '@mui/material/TextField';
+
+const options = ['Pasta', 'Hamburger', 'Salad', 'Salmon', 'Fries', 'Pumpkin Soup'];
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <Autocomplete
+ 	 disablePortal
+ 	 id="combo-box-demo"
+ 	 options={options}
+ 	 sx={{ width: 300 }}
+ 	 renderInput={(params) => <TextField {...params} label="Foods" />}
+	/>
+      <Button variant="contained">Add</Button>
       </header>
     </div>
   );
